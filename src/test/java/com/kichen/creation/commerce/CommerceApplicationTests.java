dc900b0914cf0b8b5e7442fdd937d7bc453afc1d
Jung Hwan Park <jhpark904@gmail.com>
2024-01-22 14:51:19 -0500

    save progress

--- src/test/java/com/kichen/creation/commerce/CommerceApplicationTests.java
+++ src/test/java/com/kichen/creation/commerce/CommerceApplicationTests.java
@@ -1,13 +1,12 @@
 package com.kichen.creation.commerce;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class CommerceApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
-
+	@Test
+	void contextLoads() {
+	}
 }
